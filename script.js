9fa44c55636c835ef3836d18586c06f0c20880f4
John Roller <jtroller33@gmail.com>
2022-09-27 21:16:10 -0500

    changed how its structured

--- script.js
+++ script.js
@@ -2,75 +2,35 @@ var generateBtn = document.querySelector("#generate");
 
 function writePassword(event) {
   event.preventDefault();
-
-  let lengthQuestion = window.prompt("What is the length of the string?");
+  let lengthQuestion = window.prompt("What is the length of your password?");
   console.log(lengthQuestion);
-  
-  let chooseOptions = [];
-  let questionArray = [
-    "Do you want lower case?",
-    "Do you want upper case?",
-    "Do you want numeric characters?",
-    "Do you want special characters?"
-  ];
 
-  for (let i = 0; i < questionArray.length; ++i) {
-    let userPrompt = window.prompt(questionArray[i] + "Type yes or no");
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";  
+  const numbers = "0123456789";
+  const spChar = "@%+\\/'!#$^?:,)({}[]~-_.";
 
-    if (userPrompt.toLowerCase() == "yes") {
-      chooseOptions.push(i);
-      // console.log('chooseOptions: ', chooseOptions);
-    }
+  let passwordChars = "";
+
+  let lowerCaseQuestion = window.prompt("Do you want lower case letters? Type yes or no");
+  let upperCaseQuestion = window.prompt("Do you want upper case letters? Type yes or no");
+  let numberQuestion = window.prompt("Do you want numbers? Type yes or no");
+  let spCharQuestion = window.prompt("Do you want special characters? Type yes or no");
+  if (lowerCaseQuestion.toLowerCase() == "yes") {
+    passwordChars += lowerCaseLetters;
+  } else if (upperCaseQuestion.toLowerCase() == "yes") {
+    passwordChars += upperCaseLetters;
+  } else if (numberQuestion.toLowerCase() == "yes") {
+    passwordChars += numbers;
+  } else if (spCharQuestion.toLowerCase() == "yes") {
+    passwordChars += spChar;
+  } else {
+    console.log('if/else error');
   }
-  
-  var password = generatePassword(Number(lengthQuestion), chooseOptions);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-
-  function generatePassword(lengthQuestion, array) {
-    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    const upperCaseLetters = lowerCaseLetters.toUpperCase();
-  
-    const numbers = "0123456789";
-    const spChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', '[', ']', '~', '-', '_', '.'].join("");
-  
-    let generateString = "";
-  
-    for (let i = 0; i < lengthQuestion; ++i) {
-      let randomNumberInRange = array[Math.floor(Math.random() * array.length)];
-
-      switch (randomNumberInRange) {
-      case 0:
-        generateString += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]
-        console.log(generateString)
-        break;
-      case 1:
-        generateString += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)]
-        console.log(generateString)
-        break;
-      case 2:
-        generateString += numbers[Math.floor(Math.random() * numbers.length)]
-        console.log(generateString)
-        break;
-      case 3:
-        generateString += spChar[Math.floor(Math.random() * spChar.length)]
-        console.log(generateString)
-        break;
-      default:
-        console.log("error")
-      }
-    return generateString;
-    
-    }
-  
-
-
-  }
+  console.log(passwordChars);
 }
 
-  
 
-
-// Add event listener to generate button
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 generateBtn.addEventListener("click", writePassword);
