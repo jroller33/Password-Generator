7d322df100211d2e2a566bd88dd9d88c98571e9c
John Roller <jtroller33@gmail.com>
2022-10-02 13:24:41 -0500

    everything working, incl validation

--- script.js
+++ script.js
@@ -2,33 +2,24 @@ var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 var passwordLength = 1;
-var passwordText = "";
-var passwordOptions = {
-  lowerCaseQuestion: true,
-  lowerCaseArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  upperCaseQuestion: true,
-  upperCaseArray: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  numberQuestion: true,
-  numberArray: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  specialCharsQuestion: true,
-  specialCharsArray: ["!", "#", "%", "^", "*", "+", "-", "_", "?", ".", "~"],
-}
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const spChar = "@%+\\/'!#$^?:,)({}[]~-_.";
 
-function getPasswordLength() {  // GOOD
-  // let lengthQuestion = window.prompt("What is the length of your password?");
+function getPasswordLength() {
   if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log(`pw length: ${passwordLength}`);
+    return passwordLength;
   } else {
-    passwordLength = prompt("Password length must be between 8 and 128 characters.");
+    passwordLength = prompt("What's the length of your password? (must be between 8 and 128 characters)");
     getPasswordLength();
   }
 }
 
-
-
-
 function writePassword(event) {
   event.preventDefault();
+  getPasswordLength();
+
   let lowerCaseQuestion = window.prompt("Do you want lower case letters? Type yes or no");
   let upperCaseQuestion = window.prompt("Do you want upper case letters? Type yes or no");
   let numberQuestion = window.prompt("Do you want numbers? Type yes or no");
@@ -42,15 +33,14 @@ function writePassword(event) {
     (spCharQuestion.toLowerCase() == "yes") ? (passwordChars += spChar) : "";
 
     let password = "";
-    for (let i = 0; i < getPasswordLength().lengthQuestion; i++) {
+    for (let i = 0; i < getPasswordLength(); i++) {
       password += passwordChars.charAt(
         Math.floor(Math.random() * passwordChars.length)
       );
     }
     return password;
   }
-  // passwordText.value = generatePassword();
-  console.log(generatePassword());
+  passwordText.value = generatePassword();
 }
           
 generateBtn.addEventListener("click", writePassword);
